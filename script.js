b22b6a9ed246c4f6308c219a731543630226cff1
CAMARA <88945535+camara92@users.noreply.github.com>
2025-05-19 00:55:04 +0200

    Update script.js
    upgrate scripts object

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-var carre = (nombre) => {
+// var carre = (nombre) => {
     
-    if(nombre && typeof(nombre) === 'number'){
-return nombre * nombre;
-    }else {
-        return null
-    }
+//     if(nombre && typeof(nombre) === 'number'){
+// return nombre * nombre;
+//     }else {
+//         return null
+//     }
+// }
+
+// ajout des properietes aux objets :
+
+var car = new Object();
+// on peut definir un objet comme ceci :
+// var car = {}
+// on peut aussi faire les deux en meme temps 
+var daouda_car = {
+    _id: 12345678,
+    name: "Ford",
+    annee: 2010,
+    color: "bleu"
 }
+// raojout des pro
+daouda_car.model= "Mustang";
+
+car._id = 25252525;
+car.name = "Toyata";
+car.annee = 2020;
+car.color = "rouge";
