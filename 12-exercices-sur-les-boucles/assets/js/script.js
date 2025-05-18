5f98b7648b0b2664d3ef77788e5727d1ddba9f82
CAMARA <88945535+camara92@users.noreply.github.com>
2025-05-19 00:33:02 +0200

    dernier mis a jour
    Maj

---
+++ 12-exercices-sur-les-boucles/assets/js/script.js
@@ -0,0 +1,23 @@
+// Les boucles (Exercice)
+// Table de multiplication
+
+
+for (let index = 0; index <= 12; index++) {
+    console.log(`3 x ${index} = ${index+3}`);
+}
+
+/*var index = 0
+while (index< 13) {
+    console.log("3 x "+index+" = "+index*3);
+    index++
+}*/
+
+/*var index = 0
+do {
+    // intruction
+    console.log("3 x "+index+" = "+index*3);
+
+    index++
+} while (index < 13);
+*/
+
