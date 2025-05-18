098c60442d85a702f92fcb2b9166c6c6db609851
CAMARA <88945535+camara92@users.noreply.github.com>
2025-05-19 01:06:10 +0200

    objets et ajout des proprietes en js

--- script.js
+++ script.js
@@ -9,20 +9,45 @@
 
 // ajout des properietes aux objets :
 
-var car = new Object();
-// on peut definir un objet comme ceci :
-// var car = {}
-// on peut aussi faire les deux en meme temps 
-var daouda_car = {
-    _id: 12345678,
-    name: "Ford",
-    annee: 2010,
-    color: "bleu"
-}
-// raojout des pro
-daouda_car.model= "Mustang";
+// var car = new Object();
+// // on peut definir un objet comme ceci :
+// // var car = {}
+// // on peut aussi faire les deux en meme temps 
+// var daouda_car = {
+//     _id: 12345678,
+//     name: "Ford",
+//     annee: 2010,
+//     color: "bleu"
+// }
+// // raojout des pro
+// daouda_car.model= "Mustang";
 
-car._id = 25252525;
-car.name = "Toyata";
-car.annee = 2020;
-car.color = "rouge";
+// car._id = 25252525;
+// car.name = "Toyata";
+// car.annee = 2020;
+// car.color = "rouge";
+
+var person= {
+    username: "John Doe",
+    email:"daouda.camara@gmail.com",
+    phone: "0659841730",
+    adresse: {
+        
+        street :"236 Rue du clos Lyon 69001",
+        city: "Lyon",
+        country: "France"
+    },
+    age: 30,
+    hobbies: ["reading", "cooking", "traveling"],
+    
+
+
+}
+var daouda = {
+    // le spress operateur est une sous-propriété de la personne on peut en ajouter des nouvelles proprietés et les restes vont être prise pâr Person
+    ...person,
+    username: "Daouda Camara",
+    age: 28,
+    hobbies: ["reading", "painting", "cooking"],
+    
+}
