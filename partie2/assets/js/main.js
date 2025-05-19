2921e859373d35ce2dac5d158ad2461dafc15807
CAMARA <88945535+camara92@users.noreply.github.com>
2025-05-19 02:37:42 +0200

    dom manipulation review

--- partie2/assets/js/main.js
+++ partie2/assets/js/main.js
@@ -1,3 +1,14 @@
 var html = document.getElementById('html');
 var css = document.getElementById('css');
 var js = document.getElementById('js');
+
+var classe = document.getElementsByClassName('section')
+
+var section = document.getElementsByClassName('section');
+
+var tagname = document.getElementsByTagName('input');
+
+var sections  =document.querySelectorAll('section');
+// un seul element
+var section1  =document.querySelectorAll('.section');
+console.log(section1);
