6100baac32a3f14e4110fd61d6be30f258ae0313
CAMARA <88945535+camara92@users.noreply.github.com>
2025-05-19 23:57:42 +0200

    fin partie 2
    elements dom

--- partie2/assets/js/main.js
+++ partie2/assets/js/main.js
@@ -1,14 +1,23 @@
-var html = document.getElementById('html');
-var css = document.getElementById('css');
-var js = document.getElementById('js');
+// var html = document.getElementById('html');
+// var css = document.getElementById('css');
+// var js = document.getElementById('js');
 
-var classe = document.getElementsByClassName('section')
+// var classe = document.getElementsByClassName('section')
 
-var section = document.getElementsByClassName('section');
+// var section = document.getElementsByClassName('section');
 
-var tagname = document.getElementsByTagName('input');
+// var tagname = document.getElementsByTagName('input');
 
-var sections  =document.querySelectorAll('section');
-// un seul element
-var section1  =document.querySelectorAll('.section');
-console.log(section1);
+// var sections  =document.querySelectorAll('section');
+// // un seul element
+// var section1  =document.querySelectorAll('.section');
+// console.log(section1);
+
+
+// Manipuler le contenu html 
+
+// var article = document.querySelectorAll('.article')[0];
+
+// var ajoutText = article.textContent = "Nouveau texte";
+
+// article.style.backgroundColor = "blue"
